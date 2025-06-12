b3920768f35dcca686e52979740a6d9dac980fbe
GonzaTaritolay <gonzalotaritolay94@gmail.com>
2025-06-12 14:28:54 -0300

    agregando cartas de productos usando redux

--- src/Components/ProductoCard.jsx
+++ src/Components/ProductoCard.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
+import { useSelector  } from 'react-redux';
+
 
 function ProductoCard() {
+  const { entities } = useSelector((state) => state.productos);
+ 
   return (
-    <div>ProductoCard</div>
+    <>
+      <h1>Productos</h1>
+      <div className="row">
+        {entities.map((producto) => (
+          <div className="col-md-4" key={producto.id}>
+            <div className="card mb-4">
+              <img
+                src={producto.image}
+                className="card-img-top"
+                alt={producto.title}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{producto.title}</h5>
+                <p className="card-text">${producto.price}</p>
+
+              </div>
+              <div className="card-footer">
+                <Link className="btn btn-primary" to={`/producto/${producto.id}`}>
+                  Ver producto
+                </Link>
+                <input
+                    type="checkbox"
+                    
+                  />
+                <label>favorito</label>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   )
 }
 
