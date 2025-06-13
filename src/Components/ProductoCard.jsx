0ca4df1a99952e164e73fc3b7d2721ddfbeb4f14
Franco46-ochoa <46323573@fi.unju.edu.ar>
2025-06-13 18:18:58 -0300

    Cambios de detalles

--- src/Components/ProductoCard.jsx
+++ src/Components/ProductoCard.jsx
@@ -1,46 +1,59 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+import '../Styles/Cards.css';
 
 function ProductoCard() {
   const { entities } = useSelector((state) => state.productos);
+  const [favoritos, setFavoritos] = useState({});
+
+  const toggleFavorito = (id) => {
+    setFavoritos((prev) => ({
+      ...prev,
+      [id]: !prev[id],
+    }));
+  };
 
   return (
     <>
-      <h1>Productos</h1>
-      <div className="row my-4 mx-2">
+      <h1 className="mt-4 text-center">Productos</h1>
+      <div className="container d-flex flex-wrap justify-content-center gap-4 mt-4">
         {entities.map((producto) => (
-          <div className="col-md-4" key={producto.id}>
-            <div className="card mb-4">
-              <img
-                src={producto.image}
-                className="card-img-top"
-                alt={producto.title}
-                style={{ height: "200px", objectFit: "cover" }}
+          <div
+            className="card mb-4 shadow-sm rounded-3"
+            style={{ width: '18rem' }}
+            key={producto.id}
+          >
+            <img
+              src={producto.image}
+              className="card-img-top rounded-top"
+              alt={producto.title}
+              style={{ height: '160px', objectFit: 'contain', padding: '10px' }}
+            />
+            <div className="card-body"> {/* The gradient will be applied here */}
+              <h6 className="card-title">{producto.title}</h6>
+              <p className="card-text text-muted small">${producto.price}</p>
+            </div>
+            <div className="card-footer border-0 d-flex justify-content-between align-items-center"> {/* Removed bg-white */}
+              <Link
+                className="btn btn-sm btn-primary rounded-pill px-3"
+                to={`/producto/${producto.id}`}
+              >
+                Ver producto
+              </Link>
 
-              />
-              <div className="card-body">
-                <h5 className="card-title">{producto.title}</h5>
-                <p className="card-text">${producto.price}</p>
-
-              </div>
-              <div className="card-footer">
-                <Link className="btn btn-primary" to={`/producto/${producto.id}`}>
-                  Ver producto
-                </Link>
-                <input
-                  type="checkbox"
-                  className='mx-2'
-                />
-                <label>favorito</label>
-              </div>
+              <i
+                className={`bi ${favoritos[producto.id] ? 'bi-star-fill text-warning' : 'bi-star'} fs-5`}
+                role="button"
+                onClick={() => toggleFavorito(producto.id)}
+                title="Marcar como favorito"
+              ></i>
             </div>
           </div>
         ))}
       </div>
     </>
-
-  )
+  );
 }
 
-export default ProductoCard
+export default ProductoCard;
