ba75abd3ca0ab3d41ed64eb9499eceb665ee4eaf
GonzaTaritolay <gonzalotaritolay94@gmail.com>
2025-07-03 17:47:43 -0300

    -arreglando problema de editar en formulario de producto -configuracion del hook

--- src/hooks/PrivateRoute.jsx
+++ src/hooks/PrivateRoute.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import { isLogin } from '../middleware/auth';
 
-const PrivateRoute = ({ component: Component}) => (
-    isLogin() ? <Component /> : <Navigate to="/"/>
+const PrivateRoute = ({ component: Component , ...rest }) => (
+    isLogin() ? <Component {...rest}/> : <Navigate to="/"/>
 )
 export default PrivateRoute;
